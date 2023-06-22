4cd4de82eeb358e7abef2774a1ab92429529b3e2
NatalieLarkin5 <123036205+NatalieLarkin5@users.noreply.github.com>
2023-06-21 17:05:03 -0700

    Various formatting updates

--- apps/008_placement_distance/app.py
+++ apps/008_placement_distance/app.py
@@ -26,29 +26,99 @@ if uploaded_file:
     header.columns = map(str.lower, header.columns)
     distance.columns = map(str.lower, distance.columns)
     
-    epis.index.rename('index', inplace=True)
-    st.table(epis)
+    epis['ind'] = epis.reset_index().index
+    distance['ind'] = distance.reset_index().index
 
-    epis = epis.merge(distance, on='id', how='left')
+
+    epis = epis.merge(distance, on=['id', 'ind'], validate='one_to_one')
     
-    epis["cnt"] = 1
     # tag the number of episodes
+    epis["cnt"] = 1
     epis = epis.sort_values(by = ["id","decom"])
     epis[("epi_num")] = epis["cnt"].groupby(epis["id"]).transform("cumsum")
-
+    
+    # create an indicator for first placement 
     epis["first_placement"] = epis["epi_num"] == 1
     epis = epis.merge(header, on = "id", validate = "many_to_one")   
 
-    xvar = st.selectbox('select xvar', options=('gender', 'ethnicity', 'first_placement'))  
+    # create grouping for ethnicities based on ChAT 
+    eth_groups = {"white": ["WBRI", "WIRI", "WIRT", "WORH", "WROM"], 
+                  "mixed": ["MWBC", "MWBA", "MWAS", "MOTH"] , 
+                  "asian": ["AIND", "APKN", "ABAN", "AOTH"], 
+                  "black": ["BCRB", "BAFR", "BOTH"] , 
+                  "other": ["CHNE", "OOTH"], 
+                  "dk": ["REFU", "NOBT"]}
+    epis["ethnicity_group"] = ""
+    for g in eth_groups.keys(): 
+        epis["ethnicity_group"] = np.where(epis["ethnicity"].isin(eth_groups[g]), g, epis["ethnicity_group"])
+    
+   # create grouping for placement type based on ChAT
+
+    pt_groups = {"foster": ["U1", "U2", "U3", "U4", "U5", "U6"], 
+                  "adoption": ["A3", "A4", "A5", "A6"], 
+                  "parents": ["P1"] , 
+                  "il": ["P2"], 
+                  "res_employment": ["P3"] , 
+                  "res_accom": ["H5"],
+                  "sec_ch": ["K1"],
+                  "ch": ["K2"],
+                  "res_ch": ["R1"],
+                  "nhs": ["R2"], 
+                  "young_offender": ["R3"], 
+                  "fc_mother": ["R5"], 
+                  "other": ["Z1"], 
+                   "temp": ["T0", "T1", "T2", "T3", "T4"], 
+                   "res_sch": ["S1"]}
+    
+    epis["pt_group"] = ""
+    for g in pt_groups.keys(): 
+        epis["pt_group"] = np.where(epis["place"].isin(pt_groups[g]), g, epis["pt_group"])
+ 
+
+    #st.write(epis)
+    vars = {'first_placement':"First placement?",
+            'gender':"Gender", 
+            "ethnicity": "Ethnicity",
+            "ethnicity_group": "Ethnicity group", 
+            "pt_group": "Placement  type"}
+    
+    def format_func(option):
+     return vars[option]
+    # Set labels for grouping variables 
+    labels = ["first_placement", "gender", "ethnicity", "ethnicity_group", "pt_group"] 
+    dicts  = [{True: "Yes", False : "No"},
+              {1: "Male", 2: "Female"}, 
+              {"ABAN": "Bangladeshi", "AIND" : "Indian", "AOTH": "Any other Asian",
+              "APKN": "Pakistani","BAFR": "African","BCRB": "Caribbean","Both": "Any other Black",
+              "CHNE": "Chinese","MOTH": "Any other mixed background",
+              "MWAS": "White and Asian","MWBA": "White and Black African","MWBC": "White and Black Caribbean",
+              "NOBT": "Information not available","OOTH": "Other","REFU": "Refused","WBRI": "White British", "WIRI":
+              "White Irish", "WIRT":"Traveller of Irish heritage", "WOTH":"Any other White", "WROM":"Gypsy/Roma"},
+              {"white": "White", "mixed" : "Mixed", "asian": "Asian",
+              "black": "Black","other": "Other ethnic group","dk": "Not available"}, 
+               {"foster":"Foster placement", "adoption": "Placed for adoption",  "parents": "Placed with parents", 
+               "il": "Independent living","res_employment": "Residential employment", 
+               "red_accom": "Residential accommodation", "sec_ch": "Secure Children’s Homes", 
+               "ch":"Children’s Homes", "res_ch": "Residential Care Home", 
+               "nhs":"NHS/Health Trust", "fc_mother": "Family Centre or Mother and Baby Unit", 
+               "young_offedner":"Young Offender Institution", "other": "Other type", 
+               "temp":"Temporary placement", "res_sch": "Residential school"}] 
+    
+    result = {key: val for key, val in zip(labels, dicts)}
+
+    xvar = st.selectbox('Select' , options=list(vars.keys()), format_func=format_func)
 
     dt= epis.sort_values(by = xvar)
     dt['ind'] = dt.reset_index().index
-
+    dt[xvar] = dt[xvar].astype(object)
     fig = px.scatter(dt, 
                  x = "pl_distance", 
-                 color = xvar, 
+                 color = dt[xvar].map(result[xvar]),
                  y = "ind", 
                  title = "Placement distance from home post code by episode and characteristics")
     fig.update_layout(yaxis_title = "Placement episode", 
-                      xaxis_title = "Placement distance from home post code (km)")
+                      xaxis_title = "Placement distance from home post code (km)", 
+                      legend_title_text = vars[xvar])
+    fig.update_yaxes(showticklabels=False)
     st.plotly_chart(fig)
+
