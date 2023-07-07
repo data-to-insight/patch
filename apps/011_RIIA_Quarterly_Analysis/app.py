c419e8eafae0adbbe2ea8f3ae7bf22903c772ed3
Alistair Herbert <71709086+AlistairHerbert@users.noreply.github.com>
2023-07-07 12:27:48 +0100

    Added buttons to top of page

--- apps/011_RIIA_Quarterly_Analysis/app.py
+++ apps/011_RIIA_Quarterly_Analysis/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
+st.markdown("[![Foo](https://github.com/data-to-insight/patch/blob/main/docs/img/contribute.png?raw=true)](https://www.datatoinsight.org/patch) \
+             [![Foo](https://github.com/data-to-insight/patch/blob/main/docs/img/viewthecodeimage.png?raw=true)](https://github.com/data-to-insight/patch/blob/main/apps/011_RIIA_Quarterly_Analysis/app.py)")
+
 st.title('RIIA Quarterly Analysis')
 st.write('For LAs with access to the quarterly RIIA dataset, additional analysis to complement the quarterly Excel data tool')
 
